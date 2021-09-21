b683923864040f7287fe5e2a22958279a322dfaf
Серов А. П <71006363+Error1368@users.noreply.github.com>
2021-09-21 11:57:13 +0300

    Add files via upload

---
+++ T0010/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace T0010
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sliderSize = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonGenerate = new System.Windows.Forms.Button();
+            this.buttonProcessing = new System.Windows.Forms.Button();
+            this.tableInput = new System.Windows.Forms.DataGridView();
+            this.tableOutput = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.sliderSize)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableInput)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableOutput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Входная матрица";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(429, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Выходная матрица";
+            // 
+            // sliderSize
+            // 
+            this.sliderSize.Location = new System.Drawing.Point(12, 440);
+            this.sliderSize.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.sliderSize.Name = "sliderSize";
+            this.sliderSize.Size = new System.Drawing.Size(48, 20);
+            this.sliderSize.TabIndex = 4;
+            this.sliderSize.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(75, 442);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Размер входной матрицы";
+            // 
+            // buttonGenerate
+            // 
+            this.buttonGenerate.Location = new System.Drawing.Point(12, 466);
+            this.buttonGenerate.Name = "buttonGenerate";
+            this.buttonGenerate.Size = new System.Drawing.Size(108, 23);
+            this.buttonGenerate.TabIndex = 6;
+            this.buttonGenerate.Text = "Сгенерировать";
+            this.buttonGenerate.UseVisualStyleBackColor = true;
+            this.buttonGenerate.Click += new System.EventHandler(this.buttonGenerate_Click);
+            // 
+            // buttonProcessing
+            // 
+            this.buttonProcessing.Location = new System.Drawing.Point(12, 495);
+            this.buttonProcessing.Name = "buttonProcessing";
+            this.buttonProcessing.Size = new System.Drawing.Size(108, 23);
+            this.buttonProcessing.TabIndex = 7;
+            this.buttonProcessing.Text = "Уплотнить";
+            this.buttonProcessing.UseVisualStyleBackColor = true;
+            this.buttonProcessing.Click += new System.EventHandler(this.buttonProcessing_Click);
+            // 
+            // tableInput
+            // 
+            this.tableInput.AllowUserToAddRows = false;
+            this.tableInput.AllowUserToDeleteRows = false;
+            this.tableInput.AllowUserToResizeColumns = false;
+            this.tableInput.AllowUserToResizeRows = false;
+            this.tableInput.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableInput.Location = new System.Drawing.Point(16, 30);
+            this.tableInput.Name = "tableInput";
+            this.tableInput.Size = new System.Drawing.Size(400, 400);
+            this.tableInput.TabIndex = 8;
+            // 
+            // tableOutput
+            // 
+            this.tableOutput.AllowUserToAddRows = false;
+            this.tableOutput.AllowUserToDeleteRows = false;
+            this.tableOutput.AllowUserToResizeColumns = false;
+            this.tableOutput.AllowUserToResizeRows = false;
+            this.tableOutput.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.tableOutput.Location = new System.Drawing.Point(432, 29);
+            this.tableOutput.Name = "tableOutput";
+            this.tableOutput.Size = new System.Drawing.Size(400, 400);
+            this.tableOutput.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 661);
+            this.Controls.Add(this.tableOutput);
+            this.Controls.Add(this.tableInput);
+            this.Controls.Add(this.buttonProcessing);
+            this.Controls.Add(this.buttonGenerate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.sliderSize);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "T 00 10 by Serov_a_p from g.3";
+            ((System.ComponentModel.ISupportInitialize)(this.sliderSize)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableInput)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableOutput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown sliderSize;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonGenerate;
+        private System.Windows.Forms.Button buttonProcessing;
+        private System.Windows.Forms.DataGridView tableInput;
+        private System.Windows.Forms.DataGridView tableOutput;
+    }
+}
+
---
+++ T0010/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace T0010
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGenerate_Click(object sender, EventArgs e)
+        {
+            int matrixSize = Convert.ToInt32(sliderSize.Value);
+            int cellSize = tableInput.Height / (matrixSize + 1);
+            tableInput.Columns.Clear();
+            for(int i = 0; i < matrixSize; i++)
+            {
+                DataGridViewColumn column = new DataGridViewColumn();
+                column.Frozen = true;
+                column.CellTemplate = new DataGridViewTextBoxCell();
+                column.HeaderCell = new TableColumnTumbler(tableInput, i);
+                column.Width = cellSize;
+                tableInput.Columns.Add(column);
+            }
+
+            Random randomizer = new Random();
+            for (int i = 0; i < matrixSize; i++) {
+                string[] values = new string[matrixSize];
+                for (int j = 0; j < matrixSize; j++) {
+                    string value = Convert.ToString(randomizer.Next(-5, 5));
+                    values[j] = value;    
+                }
+                tableInput.Rows.Add(values);
+                tableInput.Rows[tableInput.Rows.Count - 1].HeaderCell = new TableRowTumbler(tableInput, i);
+                tableInput.Rows[tableInput.Rows.Count - 1].Height = cellSize;
+            }
+        }
+
+        private void buttonProcessing_Click(object sender, EventArgs e)
+        {
+            int[,] matrix = new int[tableInput.RowCount, tableInput.ColumnCount];
+            for (int i = 0; i < tableInput.RowCount; i++)
+            {
+                for (int j = 0; j < tableInput.ColumnCount; j++)
+                    matrix[i, j] = Convert.ToInt32(tableInput[j, i].Value);
+            }
+
+            matrix = processMatrix(matrix);
+
+            tableOutput.Columns.Clear();
+            if (matrix.GetLength(0) > 0)
+            {
+                int cellWidth = tableOutput.Width / (matrix.GetLength(1) + 1);
+                int cellHeight = tableOutput.Height /(matrix.GetLength(0) + 1);
+                for (int i = 0; i < matrix.GetLength(1); i++)
+                {
+                    DataGridViewColumn column = new DataGridViewColumn();
+                    column.Frozen = true;
+                    column.CellTemplate = new DataGridViewTextBoxCell();
+                    column.Width = cellWidth;
+                    tableOutput.Columns.Add(column);
+                }
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    string[] values = new string[matrix.GetLength(1)];
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        string value = Convert.ToString(matrix[i, j]);
+                        values[j] = value;
+                    }
+                    tableOutput.Rows.Add(values);
+                    tableOutput.Rows[tableOutput.Rows.Count - 1].Height = cellHeight;
+                }
+            }
+    }
+
+        private int[,] processMatrix(int[,] input) {
+            List<int> deletedRows = new List<int>(), deletedColumns = new List<int>();
+            for (int i = 0; i < input.GetLength(0); i++)
+            {
+                bool isNotZeroRow = false;
+                for (int j = 0; j < input.GetLength(1); j++)
+                {
+                    if (input[i, j] != 0)
+                    {
+                        isNotZeroRow = true;
+                        break;
+                    }
+                }
+                if (!isNotZeroRow)
+                    deletedRows.Add(i);
+            }
+            if (input.GetLength(0) - deletedRows.Count > 0)
+            {
+                for (int j = 0; j < input.GetLength(1); j++)
+                {
+                    bool isNotZeroColumn = false;
+                    for (int i = 0; i < input.GetLength(0); i++)
+                    {
+                        if (input[i, j] != 0)
+                        {
+                            isNotZeroColumn = true;
+                            break;
+                        }
+                    }
+                    if (!isNotZeroColumn)
+                        deletedColumns.Add(j);
+                }
+            }
+
+            int[,] result = new int[input.GetLength(0) - deletedRows.Count, input.GetLength(1) - deletedColumns.Count];
+            int deltaI = 0;
+            for (int i = 0; i < input.GetLength(0); i++)
+            {
+                if (!deletedRows.Contains(i))
+                {
+                    int deltaJ = 0;
+                    for (int j = 0; j < input.GetLength(1); j++)
+                    {
+                        if (!deletedColumns.Contains(j))
+                        {
+                            result[i - deltaI, j - deltaJ] = input[i,j];
+                        }
+                        else
+                            deltaJ++;
+                    }
+                }
+                else
+                    deltaI++;
+            }
+            return result;
+        }
+
+        private partial class TableColumnTumbler : DataGridViewColumnHeaderCell
+        {
+            //Тумблер для зануления\заполнения рандомом конкретного столбца
+            private bool zeroLine = false;
+            private int index;
+            private DataGridView parent;
+            private static Random randomizer = new Random();
+
+            public TableColumnTumbler(DataGridView parent, int index)
+            {
+                this.parent = parent;
+                this.index = index;
+            }
+            override
+            protected void OnClick(DataGridViewCellEventArgs e)
+            {
+                if (!this.zeroLine)
+                    for (int i = 0; i < parent.RowCount; i++)
+                        parent[index, i].Value = "0";
+                else
+                    for (int i = 0; i < parent.RowCount; i++)
+                        parent[index, i].Value = Convert.ToString(randomizer.Next(-5, 5));
+                this.zeroLine = !zeroLine;
+            }
+
+        }
+
+        private partial class TableRowTumbler : DataGridViewRowHeaderCell
+        {
+            //Тумблер для зануления\заполнения рандомом конкретной строки
+            private bool zeroLine = false;
+            private int index;
+            private DataGridView parent;
+            private static Random randomizer = new Random();
+
+            public TableRowTumbler(DataGridView parent, int index)
+            {
+                this.parent = parent;
+                this.index = index;
+            }
+            override
+            protected void OnClick(DataGridViewCellEventArgs e)
+            {
+                if (!this.zeroLine)
+                    for (int i = 0; i < parent.ColumnCount; i++)
+                        parent[i, index].Value = "0";
+                else
+                    for (int i = 0; i < parent.ColumnCount; i++)
+                        parent[i, index].Value = Convert.ToString(randomizer.Next(-5, 5));
+                this.zeroLine = !zeroLine;
+            }
+
+        }
+
+    }
+}
